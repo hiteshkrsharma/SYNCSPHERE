286bef8f34a17f2dfcf783fea7e32c0d2bb95c4d
Arshad Khan <Arshadkhan627@outlook.com>
2025-01-18 15:08:34 +0530

    Logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,24 @@
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
 
   useEffect(() => {
-    if (!data && !isLoading){
+    if (!data && !isLoading) {
       router.push("/sign-in");
     }
   }, [data]);
 
   return (
     <div>
-      Only visible to authorized users.
+      <UserButton/>
     </div>
   );
 };  
