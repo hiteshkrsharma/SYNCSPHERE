1b15173a3aeb057a8c3d244ae586cf171e664b6d
Arshad Khan <Arshadkhan627@outlook.com>
2025-01-24 00:50:07 +0530

    Mobile responsive sidebar

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -4,15 +4,19 @@ import { MenuIcon } from "lucide-react";
 
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sidebar } from "./sidebar";
 
 export const MobileSidebar = () => {
     return (
         <Sheet modal={false}>
             <SheetTrigger asChild>
                 <Button size="icon" variant="secondary" className="lg:hidden">
-                    <MenuIcon />
+                    <MenuIcon className="size-4 text-neutral-500" />
                 </Button>
             </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
         </Sheet>
     );
 };
