2cba3366d9489c7f2a988d9fbb9804d853165a2f
Arshad Khan <ArshadKhan627@outlook.com>
2025-02-03 23:11:04 +0530

    Building workspace setting

---
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -0,0 +1,22 @@
+import { getCurrent } from "@/features/auth/actions";
+import { redirect } from "next/navigation";
+
+interface WorkspaceIdSettingsPageProps {
+    params: {
+        workspaceId: string;
+    };
+};
+
+const WorkspaceIdSettingsPage = async ({
+    params,
+}: WorkspaceIdSettingsPageProps) => {
+    const user = await getCurrent();
+    if (!user) redirect("/sign-in");
+    return (
+        <div>
+            WorkspaceIdSettingsPage: {params.workspaceId}
+        </div>
+    );
+};
+
+export default WorkspaceIdSettingsPage;
---
+++ src/features/workspaces/type.ts
@@ -0,0 +1,9 @@
+import { Models } from "node-appwrite";
+import { string } from "zod";
+
+export type Workspace = Models.Document & {
+    name: string;
+    imageUrl: string;
+    inviteCode: string;
+    userId: string;
+}; 
