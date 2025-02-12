fc5aecf6231031539aef3e5996b5ecc929026bf8
Arshad Khan <ArshadKhan627@outlook.com>
2025-02-12 19:25:08 +0530

    Initial commit building invite system

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 
@@ -12,6 +13,7 @@ import { MemberRole } from "@/features/members/types";
 
 import { generateInviteCode } from "@/lib/utils";
 import { getMember } from "@/features/members/utils";
+import { Workspace } from "../types";
 
 
 const app = new Hono()
@@ -209,6 +211,51 @@ const app = new Hono()
           inviteCode: generateInviteCode(6),
         },
       );
+      return c.json({ data: workspace });
+    }
+  )
+  .post(
+    "/:workspaceId/join",
+    sessionMiddleware,
+    zValidator("json", z.object({ code: z.string() })),
+    async (c) => {
+      const { workspaceId } = c.req.param();
+      const { code } = c.req.valid("json");
+
+      const databases = c.get("databases");
+      const user = c.get("user");
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if(member) {
+        return c.json({ error: "Already a member"}, 400);
+      }
+
+      const workspace = await databases.getDocument<Workspace>(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId
+      );
+
+      if (workspace.inviteCode !== code) {
+        return c.json({ error: "Invalid invite code" }, 400);
+      }
+
+      await databases.createDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        ID.unique(),
+        {
+          workspaceId,
+          userId: user.$id,
+          role: MemberRole.MEMBER,
+        },
+      );
+
       return c.json({ data: workspace });
     }
   );
