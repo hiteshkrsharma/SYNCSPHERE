20d5ae19445572b3be4a3da9566a7b853da9ad43
Arshad Khan <Arshadkhan627@outlook.com>
2025-03-14 03:17:59 +0530

    Added Kanban Update API

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,9 @@ import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { DataKanban } from "./data-kanban";
+import { useCallback } from "react";
+import { TaskStatus } from "../types";
+import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
 
 export const TaskViewSwitcher = () => {
   const [{
@@ -32,6 +35,8 @@ export const TaskViewSwitcher = () => {
     const workspaceId = useWorkspaceId();
     const { open } = useCreateTaskModal();
 
+    const { mutate: bulkUpdate } = useBulkUpdateTasks();
+
   const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
     workspaceId,
     status,
@@ -39,6 +44,14 @@ export const TaskViewSwitcher = () => {
     projectId,
     dueDate,
   });
+
+  const onKanbanChange = useCallback((
+    tasks: { $id: string; status: TaskStatus; position: number }[]
+  ) => {
+    bulkUpdate({
+      json: { tasks },
+    });
+  }, [bulkUpdate]);
 
   return (
     <Tabs
@@ -77,7 +90,7 @@ export const TaskViewSwitcher = () => {
             <DataTable columns={columns} data={tasks?.documents ?? []} />
           </TabsContent>
           <TabsContent value="kanban" className="mt-0">
-            <DataKanban data={tasks?.documents ?? []} />
+            <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []} />
           </TabsContent>
           <TabsContent value="calendar" className="mt-0">
             {JSON.stringify(tasks)}
--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -13,6 +13,8 @@ import { getMember } from "@/features/members/utils";
 import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { createAdminClient } from "@/lib/appwrite";
 import { Project } from "@/features/projects/types";
+import { error } from "console";
+
 
 const app = new Hono()
   .delete("/:taskId", sessionMiddleware, async (c) => {
@@ -314,6 +316,67 @@ const app = new Hono()
           assignee,
         },
       });
+    }
+  )
+  .post(
+    "/bulk-update",
+    sessionMiddleware,
+    zValidator(
+      "json",
+      z.object({
+        tasks: z.array(
+          z.object({
+            $id: z.string(),
+            status: z.nativeEnum(TaskStatus),
+            position: z.number().int().positive().min(1000).max(1_000_000),
+          })
+        )
+      })
+    ),
+    async (c) => {
+      const databases = c.get("databases");
+      const user = c.get("user");
+      const { tasks } = await c.req.valid("json");
+
+      const tasksToUpdate = await databases.listDocuments<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        [Query.contains("$id", tasks.map((task) => task.$id))]
+      );
+
+      const workspaceIds = new Set(tasksToUpdate.documents.map(task => task.workspaceId));
+      if (workspaceIds.size !== 1) {
+        return c.json({ error: "Tasks must belong to the same workspace" });
+      }
+
+      const workspaceId = workspaceIds.values().next().value;
+      if (!workspaceId) {
+        return c.json({ error: "Workspace ID is undefined" }, 400);
+      }
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized"}, 401);
+      }
+
+      const updatedTasks = await Promise.all(
+        tasks.map(async (task) => {
+          const { $id, status, position } = task;
+          return databases.updateDocument<Task>(
+            DATABASE_ID,
+            TASKS_ID,
+            $id,
+            { status, position }
+          );
+        })
+      );
+
+      return c.json({ data: updatedTasks });
     }
   )
 
