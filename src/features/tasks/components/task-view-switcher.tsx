5ba9aff0b84057405971c442993c4ac8edaba088
Arshad Khan <Arshadkhan627@outlook.com>
2025-03-15 01:55:47 +0530

    Calendar Data Completed

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -16,10 +16,13 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+
 import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
+
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
   const [{
@@ -92,8 +95,8 @@ export const TaskViewSwitcher = () => {
           <TabsContent value="kanban" className="mt-0">
             <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []} />
           </TabsContent>
-          <TabsContent value="calendar" className="mt-0">
-            {JSON.stringify(tasks)}
+          <TabsContent value="calendar" className="mt-0 h-full pb-4">
+            <DataCalendar data={tasks?.documents ?? []} />
           </TabsContent>
         </>
         )}
